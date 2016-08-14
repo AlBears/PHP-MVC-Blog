4557ade3dc53cde9525708e3765ae35537fd060e
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-14 16:34:55 +0300

    Simple routing, category presentation

--- controllers/PageController.php
+++ controllers/PageController.php
@@ -11,12 +11,28 @@ class PageController extends BaseController {
 		echo $this->twig->render('home.html', ['posts'=>$posts, 'categories'=>$categories]);
 	}
 
-	public function getShowPostPage() {
-		echo $this->twig->render('post.html');
+	public function getShowAllCategoriesPage()
+	{
+		$posts = Posts::all();
+		$categories = Categories::all();
+		echo $this->twig->render('posts.html', ['posts'=>$posts, 'categories'=>$categories]);
+	}
+
+	public function getShowPostPage() 
+	{
+		$uri = explode("/", $_SERVER['REQUEST_URI']);
+    	$target = $uri[2];
+    	$posts = Posts::where('id', '=', $target)->get();
+    	$categories = Categories::all();
+		echo $this->twig->render('post.html', ['posts'=>$posts, 'categories'=>$categories]);
 	}
 
 	public function getShowPostsPage() {
-		echo $this->twig->render('posts.html');
+		$uri = explode("/", $_SERVER['REQUEST_URI']);
+    	$target = $uri[2];
+    	$posts = Posts::where('category', '=', $target)->get();
+    	$categories = Categories::all();
+		echo $this->twig->render('posts.html',['posts'=>$posts, 'categories'=>$categories]);
 	}
 
 	public function getBaseTest() 
--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 
 $router->map('GET', '/', 'PageController@getShowHomePage', 'home');
-$router->map('GET', '/post', 'PageController@getShowPostPage', 'post');
-$router->map('GET', '/posts', 'PageController@getShowPostsPage', 'posts');
+$router->map('GET', '/post/[*]', 'PageController@getShowPostPage', 'post');
+$router->map('GET', '/posts', 'PageController@getShowAllCategoriesPage', 'posts');
+$router->map('GET', '/category/[*]', 'PageController@getShowPostsPage', 'categories');
 $router->map('GET', '/base', 'PageController@getBaseTest', 'db');
